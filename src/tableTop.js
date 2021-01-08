53df21a5a9238e616c06d8f2d487177cfa1ee807
George Joseph <s3752764@student.rmit.edu.au>
2021-01-08 14:01:25 +1100

    Added TableTop class.

---
+++ src/tableTop.js
@@ -0,0 +1,19 @@
+/**
+ * Class representing a table top where the toy robot is placed.
+ */
+class TableTop {
+
+    constructor (length=5, width=5) {
+        this.length = length;
+        this.width = width;
+    }
+
+    get Measurements() {
+        return {
+            length: this.length,
+            width: this.width
+        }
+    }
+}
+
+module.exports = TableTop;
