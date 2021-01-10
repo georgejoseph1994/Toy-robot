62f61a30a26b42a5f34b7161a436eeeb729f7bc0
George Joseph <s3752764@student.rmit.edu.au>
2021-01-10 14:46:13 +1100

    Refactoring robot class and tests

--- src/robot.js
+++ src/robot.js
@@ -3,17 +3,16 @@
  * Contains all the methods to manipulate the toys direction and possition.
  */
 module.exports = class robot {
-  constructor() {
-    this.tableTop = null;
+  constructor(tableTop) {
+    this.tableTop = tableTop;
     this.position = null;
   }
   /**
    * place the robot on a table top in the specified x,y possition
    * and facing the given direction.
    */
-  placeOn(tableTop, x, y, direction) {
-    if (this.isSafePossition(tableTop, x, y)) {
-      this.tableTop = tableTop;
+  placeOn(x, y, direction) {
+    if (this.isSafePossition(x, y)) {
       this.position = {
         x,
         y,
@@ -25,8 +24,13 @@ module.exports = class robot {
    *  Returns a boolean value signifying the safety of the toy
    *  in a given position.
    */
-  isSafePossition(tableTop, x, y) {
-    if (x < 0 || x > tableTop.length - 1 || y < 0 || y > tableTop.width - 1) {
+  isSafePossition(x, y) {
+    if (
+      x < 0 ||
+      x > this.tableTop.length - 1 ||
+      y < 0 ||
+      y > this.tableTop.width - 1
+    ) {
       return false;
     }
     return true;
--- test/robot.test.js
+++ test/robot.test.js
@@ -9,20 +9,17 @@ describe("A toy robot", () => {
   let robot, tableTop;
 
   beforeEach(() => {
-    robot = new Robot();
     tableTop = new TableTop(5, 5);
+    robot = new Robot(tableTop);
   });
 
   it("Should be not be placable outside a table top", () => {
-    robot.placeOn(tableTop, -1, 3, "NORTH");
+    robot.placeOn(-1, 3, "NORTH");
     expect(robot.position).to.be.deep.equal(null);
   });
 
   it("Should be placable on a table top", () => {
-    let robot = new Robot();
-    let tableTop = new TableTop(5, 5);
-
-    robot.placeOn(tableTop, 2, 3, "NORTH");
+    robot.placeOn(2, 3, "NORTH");
     expect(robot.position).to.be.deep.equal({
       x: 2,
       y: 3,
