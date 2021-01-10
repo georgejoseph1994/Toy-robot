d09a9a8b30d965006f2f6ccd2b5dc48d8f3671ce
George Joseph <s3752764@student.rmit.edu.au>
2021-01-10 16:33:34 +1100

    Added move method in Robot

--- src/robot.js
+++ src/robot.js
@@ -1,3 +1,4 @@
+const DIRECTIONS = require("./directions");
 /**
  * Class representing the Robot Toy
  * Contains all the methods to manipulate the toys direction and possition.
@@ -19,7 +20,42 @@ module.exports = class robot {
         direction,
       };
     }
+    return this.position;
   }
+
+  /**
+   * Method to move the robot one unit in the direction it is facing
+   */
+  move() {
+    if (this.position == null) {
+      return;
+    }
+    let futurePositionX = this.position.x;
+    let futurePositionY = this.position.y;
+    switch (this.position.direction) {
+      case DIRECTIONS.NORTH:
+        futurePositionY += 1;
+        break;
+      case DIRECTIONS.SOUTH:
+        futurePositionY -= 1;
+        break;
+      case DIRECTIONS.EAST:
+        futurePositionX -= 1;
+        break;
+      case DIRECTIONS.WEST:
+        futurePositionX += 1;
+        break;
+    }
+    if (this.isSafePossition(futurePositionX, futurePositionY)) {
+      this.position = {
+        x: futurePositionX,
+        y: futurePositionY,
+        direction: this.position.direction,
+      };
+    }
+    return this.position;
+  }
+
   /**
    *  Returns a boolean value signifying the safety of the toy
    *  in a given position.
