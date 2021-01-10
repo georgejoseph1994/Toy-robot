4cbafb575c13c9853ba7af454455083844e614d9
George Joseph <s3752764@student.rmit.edu.au>
2021-01-10 22:38:42 +1100

    - Cleaning up index.js - Added debug mode.

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 "use strict";
 
 const fs = require("fs");
+
 const CONFIG = require("./config.json");
-const FILE_PATH = CONFIG.filePath;
 const TableTop = require("./src/tableTop");
 const Parser = require("./src/parser");
 const Robot = require("./src/robot");
 
-let tableTop = new TableTop(CONFIG.tableLength, CONFIG.tableWidth);
+const FILE_PATH = CONFIG.filePath;
+const debug = false;
+const tableTop = new TableTop(CONFIG.tableLength, CONFIG.tableWidth);
 let robot = new Robot(tableTop);
-// console.log(tableTop.Measurements);
-
 /**
- * Entry point to the application
+ * Entry point to the application.
+ * Reads file specified in config.json and invokes
+ * command parser for the toy robot.
  */
 let driver = function () {
   fs.readFile(FILE_PATH, "utf8", function (err, data) {
     if (err) {
-      console.log("Missing input file in the input folder");
+      console.error(
+        "Warning !! Missing input file, Check filePath in config.json"
+      );
+      return;
       // put the code to read from terminal here if there is time
     }
 
+    console.log("Toy Robot Simulator");
+    console.log("===================");
+
     let commandsArray = data.split("\n");
-
     commandsArray.forEach((commandText) => {
-      Parser.parse(robot, commandText.trim().toUpperCase());
+      Parser.parseAndRun(robot, commandText.trim().toUpperCase());
+      if (debug) {
+        console.log(commandText);
+        console.log(robot.position);
+      }
     });
-
-    // console.log(data);
-    // console.log(commandsArray);
   });
 };
 
